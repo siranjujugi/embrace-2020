bd901ab0eadcf8c7609478342d85d71e65ccdc32
Kevin Adler <kadler@us.ibm.com>
2020-08-14 16:28:28 -0500

    Update to real model
    Adjust python dependencies (xgboost, scikit-learn, etc) based on those
    used by Noah to generate the model.

--- python/src/server.py
+++ python/src/server.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 
 import pickle
+import pandas as pd
 from flask import Flask, jsonify, request
 from schema import Schema, And, Or, SchemaError
 
@@ -30,24 +31,17 @@ PREDICT_SCHEMA = Schema({
 })
 
 PREDICT_KEYS = [
-    'CHARGE_COUNT',
-    'CHARGE_DISPOSITION',
     'OFFENSE_CATEGORY',
     'PRIMARY_CHARGE_FLAG',
     'DISPOSITION_CHARGED_OFFENSE_TITLE',
+    'CHARGE_COUNT',
     'DISPOSITION_CHARGED_CLASS',
+    'CHARGE_DISPOSITION',
     'SENTENCE_JUDGE',
     'SENTENCE_PHASE',
-    'COMMITMENT_TERM',
-    'COMMITMENT_UNIT',
-    'LENGTH_OF_CASE_in_Days',
     'AGE_AT_INCIDENT',
-    'RACE',
     'GENDER',
-    'INCIDENT_CITY',
-    'LAW_ENFORCEMENT_AGENCY',
-    'LAW_ENFORCEMENT_UNIT',
-    'SENTENCE_TYPE',
+    'LAW_ENFORCEMENT_AGENCY'
 ]
 
 
@@ -58,18 +52,10 @@ def predict():
     except SchemaError as error:
         return jsonify(message=str(error)), 404
 
-    # Ensure these are floats, not ints for the model
-    data['LENGTH_OF_CASE_in_Days'] = float(data['LENGTH_OF_CASE_in_Days'])
-    data['AGE_AT_INCIDENT'] = float(data['AGE_AT_INCIDENT'])
-
-    # Convert JSON to array to pass to the model
-    data = [data[k] for k in PREDICT_KEYS]
-
-    # TEMP to convert to the example model until we get the real one
-    data = [float(data[0])]
-
-    probability = model.predict([data])
+    # Ensure that the data is in the correct order for the model
+    data = {k: [data[k]] for k in PREDICT_KEYS}
+    probability = model.predict(pd.DataFrame(data))
 
     return {
-        'bias_probability': probability[0],
+        'years_of_racial_bias_sentencing_discrepency': float(probability[0]),
     }
